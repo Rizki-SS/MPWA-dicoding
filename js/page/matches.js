89e27018a1040b57c8a6013fde1e6b1e68ebe75a
isak <muhammadisak.rizki@gmail.com>
2020-10-27 17:24:27 +0700

    auto icon cange saved

--- js/page/matches.js
+++ js/page/matches.js
@@ -1,44 +1,62 @@
 import { getCompoetition } from "../api.js";
 import { saveMatchHandle } from "../saveHandle.js";
+import { getById } from "../database/db.js";
 
-const runMatches = () => {
-    getCompoetition().then((data) => {
-        const list = {
-            "headings": [
-                "Date",
-                "Status",
-                "Group.",
-                "Home Team",
-                "Winner",
-                "Away Team",
-                "#",
-            ],
-            "data": []
+const runMatches = async() => {
+    const list = {
+        "headings": [
+            "Date",
+            "Status",
+            "Group.",
+            "Home Team",
+            "Winner",
+            "Away Team",
+            "#",
+        ],
+        "data": []
+    }
+
+    const matchAll = await getCompoetition();
+    debugger;
+
+    // saya menggunakan for karena berdasarkan refrensi internet foreach tidak lah support
+    // https://itnext.io/why-async-await-in-a-foreach-is-not-working-5f13118f90d
+    for (let i = 0; i < matchAll.length; i++) {
+        const getLocalData = await getById('match', matchAll[i].id);
+
+        let btnType;
+        if (getLocalData == null) {
+            btnType = `bookmark_border`
+        } else {
+            btnType = `bookmark`
         }
 
-        data.forEach(data => {
-            list.data.push([
-                data.utcDate, data.group, data.status, data.homeTeam.name, data.score.winner, data.awayTeam.name, btn(data.id)
+        list.data.push([
+            matchAll[i].utcDate,
+            matchAll[i].group,
+            matchAll[i].status,
+            matchAll[i].homeTeam.name,
+            matchAll[i].score.winner,
+            matchAll[i].awayTeam.name,
+            btn(matchAll[i].id, btnType)
+        ])
+    }
 
-            ])
-        });
+    const dataTable = new simpleDatatables.DataTable("#tabel", {
+        perPage: 40,
+        data: list,
+        perPageSelect: false,
+    });
 
-        const dataTable = new simpleDatatables.DataTable("#tabel", {
-            perPage: 40,
-            data: list,
-            perPageSelect: false,
-        });
-
-        saveMatchHandle(data);
-        dataTable.on('datatable.page', function(page) {
-            saveMatchHandle(data);
-        });
-    })
+    saveMatchHandle(matchAll);
+    dataTable.on('datatable.page', function(page) {
+        saveMatchHandle(matchAll);
+    });
 }
 
-const btn = (data) => {
+const btn = (data, tipe) => {
     return `<button class="btn-floating btn-sm red save-match waves-effect waves-light" type="button" data-match=${data}>
-            <i class="material-icons">bookmark_border</i>
+            <i class="material-icons">${tipe}</i>
             </button>`
 }
 
--- js/page/teams.js
+++ js/page/teams.js
@@ -1,70 +1,73 @@
 import { getTims } from "../api.js";
 import btnFavHandle from "../saveHandle.js";
+import { getById } from "../database/db.js";
 
-const runTeams = () => {
-    getTims().then((data) => {
+const runTeams = async() => {
+        const data = await getTims();
         let cardList = '';
         let logoTimList = '';
-        data.forEach(e => {
-            e.crestUrl = e.crestUrl.replace(/^http:\/\//i, 'https://')
+
+        for (let i = 0; i < data.length; i++) {
+            data[i].crestUrl = data[i].crestUrl.replace(/^http:\/\//i, 'https://')
+            let btnType;
+            const dataLocal = await getById("tims", data[i].id);
+            console.log(dataLocal);
             cardList += `
             <div class="col s12 m6 l4">
                 <div class="card medium sticky-action">
                     <div class="card-image waves-effect waves-block waves-light">
-                    <img class="activator" src="${e.crestUrl}" style=" max-height: 240px">
+                    <img class="activator" src="${data[i].crestUrl}" style=" max-height: 240px">
                     </div>
                     <div class="card-content">
-                    <span class="card-title activator grey-text text-darken-4">${e.name}<i class="material-icons right">more_vert</i></span>
+                    <span class="card-title activator grey-text text-darken-4">${data[i].name}<i class="material-icons right">more_vert</i></span>
                     </div>
                     <div class="card-action">
-                        <button class="btn-floating btn-sm red btn-fav" data-tim=${e.id}>
-                            <i class="material-icons">favorite_border</i>
+                        <button class="btn-floating btn-sm red btn-fav" data-tim=${data[i].id}>
+                            ${(dataLocal == null)?`<i class="material-icons">favorite_border</i>`:`<i class="material-icons">favorite</i>`}
                         </button>
-                        <a href="${e.website}" target="blank" class="btn-floating btn-sm red">
+                        <a href="${data[i].website}" target="blank" class="btn-floating btn-sm red">
                             <i class="material-icons">insert_link</i>
                         </a>
                     </div>
                     <div class="card-reveal">
-                        ${detail(e)}
+                        ${detail(data[i])}
                     </div>
                 </div>
             </div>`
-            logoTimList += `<a class="carousel-item"><img src="${e.crestUrl}"></a>`;
-        });
+        logoTimList += `<a class="carousel-item"><img src="${data[i].crestUrl}"></a>`;
+    }
 
-        document.querySelector('#team-list').innerHTML = cardList;
-        document.querySelector('#logo-tim').innerHTML = logoTimList;
+    document.querySelector('#team-list').innerHTML = cardList;
+    document.querySelector('#logo-tim').innerHTML = logoTimList;
 
-
-        var elems = document.querySelectorAll('.carousel');
-        var instances = M.Carousel.init(elems);
-        btnFavHandle(data);
-    })
+    var elems = document.querySelectorAll('.carousel');
+    var instances = M.Carousel.init(elems);
+    btnFavHandle(data);
 }
 
-const detail = (e) => {
+const detail = (data) => {
     return `
-        <span class="card-title grey-text text-darken-4">${e.name}<i class="material-icons right">close</i></span>
+        <span class="card-title grey-text text-darken-4">${data.name}<i class="material-icons right">close</i></span>
         <table>
             <tr>
                 <td>address</td>
-                <td>${e.address}</td>
+                <td>${data.address}</td>
             </tr>
             <tr>
                 <td>phone</td>
-                <td>${e.phone}</td>
+                <td>${data.phone}</td>
             </tr>
             <tr>
                 <td>website</td>
-                <td><a href="${e.website}">${e.website}</a></td>
+                <td><a href="${data.website}">${data.website}</a></td>
             </tr>
             <tr>
                 <td>email</td>
-                <td>${e.email}</td>
+                <td>${data.email}</td>
             </tr>
             <tr>
                 <td>founded</td>
-                <td>${e.founded}</td>
+                <td>${data.founded}</td>
             </tr>
         </table>
     `
